ff3570e253a28085dbe7f53807abebd07623f868
Evandir de Souza <evandirs@yahoo.com.br>
2022-08-13 20:30:48 -0300

    Atividade 07 - Criação de 2 componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+import { BackgroundComponent } from './background/background.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogoComponent,
+    BackgroundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
